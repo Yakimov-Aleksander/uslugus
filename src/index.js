6c1a4db0476a179ce9b255a4bac793122d1487dd
Yakimov-Aleksander <sasha.yakimoff@gmail.com>
2022-10-12 17:03:03 +0300

    day 5 js modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './index.html';
 import './index.scss';
+import {modalController} from "./modules/modalController"
 
+const init = () => {
+  modalController({
+    modal: '.modal_sign-in',
+    btnOpen: '.header__auth-btn_sign-in',
+    btnClose: '.modal__close'
+  });
+
+  modalController({
+    modal: '.modal_sign-up',
+    btnOpen: '.header__auth-btn_sign-up',
+    btnClose: '.modal__close'
+  });
+
+  const modalPerson = modalController({
+    modal: '.modal_person',
+    btnOpen: '.services',
+    parrentBtns: '.services__list',
+    btnClose: '.modal__close',
+    handlerOpenModal: async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json());
+
+      console.log(data);
+    }
+  });
+
+};
+
+init();
