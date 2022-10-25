ddaa61966ac35ee4b9c780070bd8afdf63dbe480
Yakimov-Aleksander <sasha.yakimoff@gmail.com>
2022-10-25 22:42:38 +0300

    finish

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,4 +1,5 @@
 import {store} from "./store";
+import {API_URL} from "./const";
 
 export const auth = (data) => {
   store.user.name = data.name
@@ -6,6 +7,19 @@ export const auth = (data) => {
   store.user.id = data.id
   store.user.avatar = data.avatar
 
-  console.log(store.user);  // todo сделать авторизацию
+  const headerAuth = document.querySelector('.header__auth');
+
+  headerAuth.classList.add('auth');
+
+  const categoryRus = store.category.find(
+    item => item.title === store.user.category
+  ).rus;
+
+  headerAuth.innerHTML = `
+    <img src="${API_URL}/${store.user.avatar}" alt="" class="auth__avatar">
+    <p class="auth__name">${store.user.name}</p>
+    <p class="auth__category">${categoryRus}</p>
+    <button class="auth__btn-edit">Изменить услугу</button>
+  `;
 }
 
